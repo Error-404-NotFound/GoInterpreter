f3fde1e726d430e7d97d9295c62d39294e4dc274
Johri Aniket Manish <johrianiket04@gmail.com>
2023-11-01 17:23:51 +0530

    Token Added

---
+++ src/gointerpreter/Token.java
@@ -0,0 +1,21 @@
+package gointerpreter;
+
+import java.lang.*;
+
+public class Token {
+    final TokenType tokenType;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    public Token(TokenType tokenType, String lexeme, Object literal, int line) {
+        this.tokenType = tokenType;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return tokenType + " " + lexeme + " " + literal;
+    }
+}
