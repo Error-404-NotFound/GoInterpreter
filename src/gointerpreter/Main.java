f406251485a0508360e6689327809abad2f7217f
Johri Aniket Manish <johrianiket04@gmail.com>
2023-11-01 22:49:18 +0530

    Token printing done

--- src/gointerpreter/Main.java
+++ src/gointerpreter/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.String;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,13 +16,17 @@ public class Main {
         BufferedReader reader = new BufferedReader(input);
         for(;;) {
             System.out.print("> ");
+            String line = reader.readLine();
+            if(line==null) break;
+            run(line);
         }
     }
 
     private static void run(String source) throws IOException {
         Scanner scanner = new Scanner(source);
-        for(Token token: token) {
-
+        List<Token> tokens = scanner.scanTokens();
+        for(Token token: tokens) {
+            System.out.println(token);
         }
     }
 }
--- src/gointerpreter/Scanner.java
+++ src/gointerpreter/Scanner.java
@@ -18,10 +18,36 @@ public class Scanner {
         this.source = source;
     }
 
+    List<Token> scanTokens() {
+        while(!isAtEnd()) {
+            start=pos_index;
+            scanToken();
+        }
+        tokens.add(new Token(TokenType.EOF,"",null,line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch(c) {
+            case '(':addToken(TokenType.LEFT_PAREN);break;
+            case ')':addToken(TokenType.RIGHT_PAREN);break;
+            case '{':addToken(TokenType.LEFT_BRACE);break;
+            case '}':addToken(TokenType.RIGHT_BRACE);break;
+            case ',':addToken(TokenType.COMMA);break;
+            case '.':addToken(TokenType.DOT);break;
+            case '-':addToken(TokenType.MINUS);break;
+            case '+':addToken(TokenType.PLUS);break;
+            case ';':addToken(TokenType.SEMICOLON);break;
+            case '/':addToken(TokenType.SLASH);break;
+            case '*':addToken(TokenType.STAR);break;
+            case '"':string();break;
+        }
+    }
+
     //////////////////////////////////////////////////////////
     //      Creating Literals
     //////////////////////////////////////////////////////////
-
 
     //Number Literal
     private void number() {
