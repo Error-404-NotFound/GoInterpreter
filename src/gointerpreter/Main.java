6f9fa31b2850862183e419cc95d52fdac06e52f0
Johri Aniket Manish <johrianiket04@gmail.com>
2023-11-01 22:20:50 +0530

    Scanner.java updated

--- src/gointerpreter/Main.java
+++ src/gointerpreter/Main.java
@@ -1,7 +1,27 @@
 package gointerpreter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.String;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+        for(;;) {
+            System.out.print("> ");
+        }
+    }
+
+    private static void run(String source) throws IOException {
+        Scanner scanner = new Scanner(source);
+        for(Token token: token) {
+
+        }
     }
 }
--- src/gointerpreter/Scanner.java
+++ src/gointerpreter/Scanner.java
@@ -2,6 +2,7 @@ package gointerpreter;
 
 import java.lang.String;
 import java.lang.Object;
+import java.lang.Double;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,13 +10,46 @@ public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<Token>();
     private int start = 0;
-    private int current = 0;
+    private int pos_index = 0;
     private int line = 1;
 
+    //Constructor for Scanner Class
     public Scanner(String source) {
         this.source = source;
     }
 
+    //////////////////////////////////////////////////////////
+    //      Creating Literals
+    //////////////////////////////////////////////////////////
+
+
+    //Number Literal
+    private void number() {
+        while(isDigit(peek())) advance();
+        //for checking floating point number
+        if(peek()=='.' && isDigit(peekNext())) {
+            advance();
+            while(isDigit(peek())) advance();
+        }
+        addToken(TokenType.NUMBER,Double.parseDouble(source.substring(start,pos_index)));
+    }
+
+    //String Literal
+    private void string() {
+
+    }
+
+    //Identifier Literal
+    private void identifier() {
+
+    }
+
+    //////////////////////////////////////////////////////////
+    //      End of Creating Literals
+    //////////////////////////////////////////////////////////
+
+
+    //Different Methods for different purposes
     private boolean isDigit(char c) {
         return c>='0' && c<='9';
     }
@@ -28,31 +62,37 @@ public class Scanner {
         return isDigit(c) || isAlphabet(c);
     }
 
+    //gives the char at the current position and increases the position index
     private char advance() {
-        current++;
-        return source.charAt(current-1);
+        pos_index++;
+        return source.charAt(pos_index-1);
     }
 
+    //checks if pos_index is at the end
     private boolean isAtEnd() {
-        return current>=source.length();
+        return pos_index>=source.length();
     }
 
+    //gives the character at pos_index
     private char peek() {
         if(isAtEnd()) return '\0';
-        return source.charAt(current);
+        return source.charAt(pos_index);
     }
 
+    //gives the charcter at pos_index+1
     private char peekNext() {
-        if(current+1>=source.length()) return '\0';
-        return source.charAt(current+1);
+        if(pos_index+1>=source.length()) return '\0';
+        return source.charAt(pos_index+1);
     }
 
+    // Make a token and make it an object and add it to the tokens list and add literal associated to it
     private void addToken(TokenType tokenType, Object literal) {
-        String token = source.substring(start,current);
-        tokens.add(new Token(tokenType,token,literal,line));
+        String token_text = source.substring(start,pos_index);
+        tokens.add(new Token(tokenType,token_text,literal,line));
     }
 
     // Polymorphism
+    // Used to add Token
     private void addToken(TokenType tokenType) {
         addToken(tokenType,null);
     }
